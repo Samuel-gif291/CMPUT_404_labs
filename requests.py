0027b4aec9f28da4fb582813e1697eeea12fbce6
Samuel-gif291 <73078870+Samuel-gif291@users.noreply.github.com>
2022-09-14 19:50:21 -0600

    Update requests.py

--- requests.py
+++ requests.py
@@ -1,6 +1,6 @@
 "This version of python prints the version of the requests library"
 
 import requests
-temp = requests.get("")
-print(temp)
+temp = requests.get("https://raw.githubusercontent.com/Samuel-gif291/CMPUT_404_labs/main/requests.py")
+print(temp.text)
 #print(requests.__version__)
